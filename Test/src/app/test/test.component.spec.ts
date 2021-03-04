6be303b78e7cd837b3d133e4a50977d96c3882ce
Alejandro <alejandro.martin.perez.99@gmail.com>
2021-03-04 10:21:50 +0100

    Resto de test

--- Test/src/app/test/test.component.spec.ts
+++ Test/src/app/test/test.component.spec.ts
@@ -13,10 +13,45 @@ describe('Cada libro tiene que tener un coste inicial de 8€', () => {
 });
 
 describe('Si se adquieren dos libros distintos se aplica un 5% de descuento en ambos', () =>{
-  let component; TestComponent;
+  let component: TestComponent;
 
   it('Si la cantidad de libros es > 1, se aplica un 5% de descuento', () => {
     component = new TestComponent;
+    component.comprarLibros(2);
+    expect(component.getPrecioDescuento()).toBe(7.6);
+  });
+
+});
+
+describe('Si se adquieren 3 libros distintos se aplica un 10% de descuento en ambos', () =>{
+  let component: TestComponent;
+
+  it('Si la cantidad de libros es > 2, se aplica un 10% de descuento', () => {
+    component = new TestComponent;
+    component.comprarLibros(3);
+    expect(component.getPrecioDescuento()).toBe(7.2);
+  });
+
+});
+
+describe('Si se adquieren 4 libros distintos se aplica un 20% de descuento en ambos', () =>{
+  let component: TestComponent;
+
+  it('Si la cantidad de libros es > 3, se aplica un 20% de descuento', () => {
+    component = new TestComponent;
+    component.comprarLibros(4);
+    expect(component.getPrecioDescuento()).toBe(6.4);
+  });
+
+});
+
+describe('Si se adquieren 5 libros distintos se aplica un 25% de descuento en ambos', () =>{
+  let component: TestComponent;
+
+  it('Si la cantidad de libros es 5, se aplica un 25% de descuento', () => {
+    component = new TestComponent;
+    component.comprarLibros(5);
+    expect(component.getPrecioDescuento()).toBe(6);
   });
 
 });
--- Test/src/app/test/test.component.ts
+++ Test/src/app/test/test.component.ts
@@ -9,17 +9,41 @@ export class TestComponent implements OnInit {
 
   coste: number;
   cantidad: number;
+  precioDescuento: number;
 
   constructor() {
     this.coste = 8;
     this.cantidad = 1;
+    this.precioDescuento = 0;
   }
 
   ngOnInit(): void {
   }
 
-  getCoste = () =>{
+  getCoste = () => {
     return this.coste;
+  }
+
+  getPrecioDescuento = () => {
+    return this.precioDescuento;
+  }
+
+  comprarLibros = (cuantos) => {
+    this.cantidad = cuantos;
+    switch (this.cantidad) {
+      case 2:
+        this.precioDescuento = (this.coste * 0.05);
+        break;
+      case 3:
+        this.precioDescuento = (this.coste * 0.1);
+        break;
+      case 4:
+        this.precioDescuento = (this.coste * 0.20);
+        break;
+      case 5:
+        this.precioDescuento = (this.coste * 0.25);
+        break;
+    }
   }
 
 }
